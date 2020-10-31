e34644677b1b781e175c8106817939d0f46092d5
Ana Tertuliano <ana.tertu.2019@gmail.com>
2020-10-31 19:01:03 -0300

    JS - onkeyup, cadastro de cliente

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/*também é possível ser somente valida_campo(){}*/
+function valida_campos() {
+    let nome = document.getElementById('nome').value;
+    let email = document.getElementById('email').value;
+    let senha = document.getElementById('senha').value;
+
+    console.log(nome, email, senha);
+}
--- php1/cadastro_cliente.php
+++ php1/cadastro_cliente.php
@@ -4,30 +4,32 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-        <link rel="stylesheet" type="text/css" href="./css/estilo.css">
-        <title>Projeto Social Ubertulho</title>
+        <link rel="stylesheet" type="text/css" href="../css/estilo.css">
+        <script src="../js/script.js"></script>
+        <title>Projeto Social Ubertulho - Cadastro de clientes</title>
 
     </head>
     <body>
         <div class="catainer">
             <div class="row">
                 <div class="col-4 mx-auto mt-6 pt-4 container-login"> <!--mt = margin top, pt = padding top-->
-                    <span class="mb-4 h3">Login</span>
+                    <span class="mb-4 h3">Cadastro de clientes</span>
                     <form method="POST" action="">
                         <div class="form-group">
+                            <label class="label-form" for="nome">Nome: </label>
+                            <input class="form-control" type="text" name="nome" id="nome" onkeyup="valida_campos">
+                        </div>
+                        <div class="form-group">
                             <label class="label-form" for="email">E-mail: </label>
-                            <input class="form-control" type="email" name="email" id="email">
+                            <input class="form-control" type="email" name="email" id="email" onkeyup="valida_campos">
                         </div>
                         <div class="form-group">
                             <label  class="label-form" for="senha">Senha: </label>
-                            <input class="form-control" type="password" name="senha" id="senha">
+                            <input class="form-control" type="password" name="senha" id="senha" onkeyup="valida_campos">
                         </div>
                         <div class="row">
                             <div class="col-1">
-                                <button type="submit" class="btn btn-primary mb-4" name="logar"> Entrar </button>
-                            </div>
-                            <div class="col-1">
-                                <a href="./php1/cadastro_cliente.php" type="submit" class="btn btn-success mb-4" name="logar"> Cadastrar </a>
+                                <a href="#" class="btn btn-success disabled mb-4" name="cadastrar" id="cadastrar-cliente"> Cadastrar </a>
                             </div>
                         </div>
                     </form>
@@ -36,3 +38,6 @@
         </div>
 </body>
 </html>
+<!--<div class="col-1">
+                                <button type="submit" class="btn btn-primary mb-4" name="logar"> Cadastrar</button>
+                            </div>-->
