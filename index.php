66cee06595b806c34248ea8a0ce1362bb87a78b0
Ana Tertuliano <ana.tertu.2019@gmail.com>
2020-10-31 14:15:35 -0300

    Php, Bootstrap, Css

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="styleshee" type="text/css" href="./css/estilo.css">
+    <title>Projeto Social Ubertulho</title>
+
+</head>
+<body>
+    <div class="catainer">
+        <div class="row">
+            <div class="col-6">
+               <form method="POST" action="">
+                    <div class="form-group">
+                        <label class="label-form" for="email">E-mail: </label>
+                        <input class="form-control" type="email" name="email" id="email">
+                    </div>
+                    <div class="form-group">
+                        <label  class="label-form" for="senha">Senha: </label>
+                        <input class="form-control" type="senha" name="senha" id="senha">
+                    </div>
+                    <button type="submit" class="btn btn-primary" name="entrar"> Entrar </button>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
